613aa8385f9c5d2b8678bb9903763db5606ebf5f
XPichasaca <72839788+XPichasaca@users.noreply.github.com>
2025-12-06 15:38:44 -0500

    Actualizar proyecto con cambios recientes 6/14

--- public/PartidosEnVivo.js
+++ public/PartidosEnVivo.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const SUPABASE_URL = "https://ghstgwywcaxtfdyyjxli.supabase.co";
 const SUPABASE_KEY = "sb_publishable_bm3rEZ92WLzBkxqpvWCu0w_oG4Cr9YZ";
